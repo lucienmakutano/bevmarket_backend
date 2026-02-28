00d178f717787b09cfb6bf8f9080d07d44628171
Lucien Makutano <lucien.programmer@gmail.com>
2026-02-28 20:16:24 +0300

    Remove conditional enabling of plpgsql extension from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_02_02_105748) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql' unless extension_enabled?('plpgsql')
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
